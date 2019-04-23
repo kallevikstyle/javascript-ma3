4689f2f7f303b6d3d7dd4ed76874f8265d61bfab
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-04-23 15:59:01 +0200

    Started level 1 question 1

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,9 @@
 //Use RegEx to validate form
+function validateInfo() {
+	let firstName = document.getElementById('firstname').value,
+		lastName = document.getElementById('lastname').value,
+		phoneNumber = document.getElementById('phone').value,
+		email = document.getElementById('email').value;
+
+	console.log(firstName, lastName, phoneNumber, email);
+}
