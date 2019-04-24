1ea767d5ebf9a6264a6e50a956dc38409369120c
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-04-24 15:45:04 +0200

    completed level1 question1

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,9 +1,29 @@
-//Use RegEx to validate form
 function validateInfo() {
-	let firstName = document.getElementById('firstname').value,
-		lastName = document.getElementById('lastname').value,
-		phoneNumber = document.getElementById('phone').value,
-		email = document.getElementById('email').value;
+	const regExPhone = /^[0-9]{8}$/,
+		  regExEmail = /^[a-z0-9.-_]+@[a-z0-9-]+\.[a-z.]{2,5}$/i;
+	let validHuman = {
+		"first name": 		(document.getElementById('firstname').value !== ""),
+		"last name": 		(document.getElementById('lastname').value !== ""),
+		"phone number": 	regExPhone.test(document.getElementById('phone').value),
+		"e-mail": 			regExEmail.test(document.getElementById('email').value)  
+	},
+		invalidValues = [],
+		i = 0;
+	
+	// loop through validHuman object to find any false values
+	for (key in validHuman) {
+		if (!validHuman[key]) {	// Add any false values to an array 
+			invalidValues.push(key);
+		} 
+	}
 
-	console.log(firstName, lastName, phoneNumber, email);
+	// console log an error message for every invalid value
+	if (invalidValues.length > 0) {
+		for (i = 0; i < invalidValues.length; i += 1) {
+			console.log("You have entered an invalid " + invalidValues[i] + ".");
+		} 
+	} else {
+		console.log("Everything looks good! Thank you for subscribing!");
+	}
+	
 }
